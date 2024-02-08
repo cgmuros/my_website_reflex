1f4d242cc667eede2c4c15f6d07426cca1fb7ef4
Cristian Munoz <cgmuros@gmail.com>
2024-02-08 17:11:50 -0300

    Add API URL to config

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="personal_website",
+    api_url="https://my-website-reflex.vercel.app"
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://cmuros.reflex.run",
