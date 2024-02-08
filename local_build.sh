24331fbe36952035565329cf7640c6050e613d15
Cristian Munoz <cgmuros@gmail.com>
2024-02-08 16:10:20 -0300

    Update API_URL in build scripts

--- local_build.sh
+++ local_build.sh
@@ -2,7 +2,7 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
-API_URL=https://mywebsitereflex-production.up.railway.app/ reflex export --frontend-only
+reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -rf frontend.zip
