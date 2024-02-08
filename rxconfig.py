dcf5c2675a4ae3a5404c32f6dd75f0841e69577e
Cristian Munoz <cgmuros@gmail.com>
2024-02-08 17:15:46 -0300

    Update API URL port in rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="personal_website",
-    api_url="https://my-website-reflex.vercel.app",
+    api_url="https://my-website-reflex.vercel.app:8000",
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://cmuros.reflex.run",
