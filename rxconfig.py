41b5b6cf85905827f0e9947cce75b90fbd646ec5
Cristian Munoz <cgmuros@gmail.com>
2024-02-08 15:17:14 -0300

    Update cors_allowed_origins in rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,9 +3,8 @@ import reflex as rx
 config = rx.Config(
     app_name="personal_website",
     cors_allowed_origins=[
-        "https://my-website-reflex.vercel.app/",
         "http://localhost:3000",
         "https://cmuros.reflex.run",
-        "https://schoolbusbackend-production.up.railway.app/"
+        "https://my-website-reflex.vercel.app/",
     ],
 )
