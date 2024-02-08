86975721b06b6b6b07d4c3677f7bd6f675cde22e
Cristian Munoz <cgmuros@gmail.com>
2024-02-08 17:42:04 -0300

    Refactor live function and update API_URL

--- rxconfig.py
+++ rxconfig.py
@@ -2,10 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="personal_website",
-    api_url="https://my-website-reflex.vercel.app:8000",
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://cmuros.reflex.run",
-        "https://my-website-reflex.vercel.app"
+        "https://my-website-reflex.vercel.app",
+        "https://mywebsitereflex-production.up.railway.app"
     ],
 )
