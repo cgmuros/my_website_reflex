de6c7d862c1f0ee5b6c946f5bd0093822aa4640c
Cristian Munoz <cgmuros@gmail.com>
2024-02-08 13:16:35 -0300

    Add production backend URL to config

--- rxconfig.py
+++ rxconfig.py
@@ -6,5 +6,6 @@ config = rx.Config(
         "https://my-website-reflex.vercel.app/",
         "http://localhost:3000",
         "https://cmuros.reflex.run",
+        "https://schoolbusbackend-production.up.railway.app/"
     ],
 )
