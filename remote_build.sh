cf0b7b00401b0dafb05d7fba13213e38732f529a
Cristian Munoz <cgmuros@gmail.com>
2025-01-15 11:31:59 -0300

    Added export command

--- remote_build.sh
+++ remote_build.sh
@@ -3,7 +3,8 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
-# API_URL=https://mywebsitereflex-production.up.railway.app reflex export --frontend-only
+# API_URL=https://mywebsitereflex-production.up.railway.app 
+reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -rf frontend.zip
