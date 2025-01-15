8384ac2ef8f16957e077920b07aab8da2fa3e265
Cristian Munoz <cgmuros@gmail.com>
2025-01-15 17:33:14 -0300

    Removed state

--- personal_website/api/api.py
+++ personal_website/api/api.py
@@ -4,16 +4,16 @@ import requests
 
 
 
-async def live() -> bool:
-    chile_tz = pytz.timezone('Chile/Continental')
-    now = datetime.datetime.now(tz=chile_tz)
-    start_time = now.replace(hour=9, minute=0, second=0, microsecond=0)
-    end_time = now.replace(hour=17, minute=0, second=0, microsecond=0)
+# async def live() -> bool:
+#     chile_tz = pytz.timezone('Chile/Continental')
+#     now = datetime.datetime.now(tz=chile_tz)
+#     start_time = now.replace(hour=9, minute=0, second=0, microsecond=0)
+#     end_time = now.replace(hour=17, minute=0, second=0, microsecond=0)
     
-    if start_time <= now <= end_time:
-        return True
-    else:
-        return False
+#     if start_time <= now <= end_time:
+#         return True
+#     else:
+#         return False
 
 
 # async def live_outside() -> bool:
--- personal_website/state/PageState.py
+++ personal_website/state/PageState.py
@@ -1,9 +1,9 @@
 import reflex as rx
-import personal_website.api.api as api
+# import personal_website.api.api as api
 
 
-class PageState(rx.State):
-    is_live : bool
+# class PageState(rx.State):
+#     is_live : bool
 
-    async def check_live(self):
-        self.is_live = await api.live()
+#     async def check_live(self):
+#         self.is_live = await api.live()
